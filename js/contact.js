6e55a80ef4a7e7401f9a920de663a19959da59bb
zakiaafrin <zjeme@techlaunch.io>
2020-04-30 23:22:27 -0400

    Updated

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,19 @@
-$('#contact-form').submit(function (e) {
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let message = document.getElementById('message');
+// $('#contact-form').submit(function (e) {
+//     let name = document.getElementById('name');
+//     let email = document.getElementById('email');
+//     let message = document.getElementById('message');
 
-    if (!name.value || !email.value || !message.value) {
-        alerttify.error('Please check your entries');
-    } else {
-        e.preventDefault();
-        $(this).get(0).reset();
-        alerttify.success('Message sent');
-    }
-})
+//     if (!name.value || !email.value || !message.value) {
+//         document.write('Please check your entries');
+//     } else {
+//         $.ajax({
+//             url: "https://formspree.io/zakia_afrin@yahoo.com",
+//             method: "POST",
+//             data: $(this).serialize(),
+//             dataType: "json"
+//         })
+//         e.preventDefault();
+//         $(this).get(0).reset();
+//         document.write('Message sent');
+//     }
+// })
